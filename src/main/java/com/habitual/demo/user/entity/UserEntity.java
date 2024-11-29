867dfb9f1943b1db064285831649d1e592daf3a2
姬媛媛 <wx-jiyy@hfvast.com>
2024-11-29 17:04:21 +0800

    添加表备注

--- src/main/java/com/habitual/demo/user/entity/UserEntity.java
+++ src/main/java/com/habitual/demo/user/entity/UserEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
 import java.io.Serial;
 
@@ -17,6 +18,7 @@ import java.io.Serial;
 @Setter
 @Entity
 @Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
+@Comment("用户")
 public class UserEntity extends BaseEntity {
 
     /**
@@ -29,51 +31,61 @@ public class UserEntity extends BaseEntity {
      * 登录账号
      */
     @Column(unique = true, nullable = false)
+    @Comment("登录账号")
     private String username;
 
     /**
      * 登录密码
      */
+    @Comment("登录密码")
     private String password;
 
     /**
      * 用户名
      */
+    @Comment("用户名")
     private String nickname;
 
     /**
      * 头像
      */
+    @Comment("头像")
     private String avatar;
 
     /**
      * 性别
      */
+    @Comment("性别")
     private String sex;
 
     /**
      * 年龄
      */
+    @Comment("年龄")
     private Long age;
 
     /**
      * 邮箱
      */
+    @Comment("邮箱")
     private String email;
 
     /**
      * 电话号
      */
+    @Comment("电话号")
     private String tel;
 
     /**
      * 角色
      */
+    @Comment("角色")
     private String role;
 
     /**
      * 状态 0启用 1禁用
      */
+    @Comment("状态 0启用 1禁用")
     private Long status;
 
 }
