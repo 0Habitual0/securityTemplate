896bc6257b948c4db87808b26cfdd29167845b15
0Habitual0 <1929774389@qq.com>
2024-12-10 13:35:12 +0800

    旅游攻略

--- src/main/java/com/habitual/demo/touristGuide/controller/TouristGuideController.java
+++ src/main/java/com/habitual/demo/touristGuide/controller/TouristGuideController.java
@@ -1,7 +1,11 @@
 package com.habitual.demo.touristGuide.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.habitual.demo.common.entity.CommonResponse;
+import com.habitual.demo.touristGuide.entity.TouristGuideEntity;
+import com.habitual.demo.touristGuide.entity.dto.TouristGuidePageDto;
+import com.habitual.demo.touristGuide.service.TouristGuideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 控制层 旅游攻略
@@ -9,4 +13,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/touristGuide")
 public class TouristGuideController {
+
+    @Autowired
+    private TouristGuideService touristGuideService;
+
+    /**
+     * 新增/修改
+     */
+    @PostMapping("save")
+    public CommonResponse save(@RequestBody TouristGuideEntity input) {
+        return touristGuideService.save(input);
+    }
+
+    /**
+     * 删除
+     */
+    @GetMapping("delete")
+    public CommonResponse delete(@RequestParam Long id) {
+        return touristGuideService.delete(id);
+    }
+
+    /**
+     * 分页查询
+     */
+    @PostMapping("selectByPage")
+    public CommonResponse selectByPage(@RequestBody TouristGuidePageDto input) {
+        return touristGuideService.selectByPage(input);
+    }
+
+    /**
+     * 收藏分页查询
+     */
+    @PostMapping("selectByPageCollect")
+    public CommonResponse selectByPageCollect(@RequestBody TouristGuidePageDto input) {
+        return touristGuideService.selectByPageCollect(input);
+    }
+
 }
